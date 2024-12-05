e97ee7045314c591efcc8176e4faba1f9d609a9d
Qobiljon <madrahimovq@gmail.com>
2024-12-05 15:07:34 +0500

    Employee classini yozdik

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ def calculate(a: int, b: int):
     return a + b
 
 print(calculate(5, 4))
+
+
+class Employee:
+    def __init__(self, name, lastname, salary):
+        self.name = name
+        self.lastname = lastname
+        self.salary = salary
